76f3b0c46ff65de0184dd4eccf213d384150f186
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-11-28 21:12:16 +0200

    Update get_categories.php

--- api/categories/get_categories.php
+++ api/categories/get_categories.php
@@ -12,6 +12,7 @@ if (!$result) {
     die("Query failed: " . $conn->error);
 }
 
+echo "<option value=''>-- Select --</option>";
 while ($c = $result->fetch_assoc()) {
     echo "<option value='{$c['id']}'>{$c['name']}</option>";
 }
